292396e19094608a80423496d25784968366498e
Liu Yihao <154865817+vectorprogrammer@users.noreply.github.com>
2025-11-30 17:13:32 +0800

    Update main.js

--- behavior_packs/scripts/main.js
+++ behavior_packs/scripts/main.js
@@ -1,7 +1,16 @@
-import { world } from "@minecraft/server";
+import { system, world, BlockComponentRegistry } from "@minecraft/server";
 import "./pale_oak_growth_drops";
 import "./ore_regeneration";
-import "./netherite_crafter_components";
+import {NetheriteCrafterComponent} from "./netherite_crafter_components";
 world.afterEvents.worldIntialize.subscribe(()=>{
-    world.sendMessage("Scripting API initalized");
-});
+    world.sendMessage("Scripting API fully initalized");
+})
+const CUSTOM_COMPONENT_ID = "convertedjava:netherite_crafter_script:crafter_component"
+system.beforeEvents.worldInitalize.subscribe(initEvent => {
+    try {
+        initEvent.blockComponentRegistry.registerCustomComponent(CUSTOM_COMPONENT_ID, new NetheriteCrafterComponent())
+        world.sendMessage("Scripting API first step initialized.");
+    } catch (e) {
+        world.sendMessage("Scripting API first step unsuccessful. Continuing...");
+    }
+}
