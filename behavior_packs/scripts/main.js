68382690ba990308e11429a3dd2c48ad84463d4b
Liu Yihao <154865817+vectorprogrammer@users.noreply.github.com>
2025-11-30 16:40:09 +0800

    Update main.js

--- behavior_packs/scripts/main.js
+++ behavior_packs/scripts/main.js
@@ -1,6 +1,7 @@
 import { world } from "@minecraft/server";
 import "./pale_oak_growth_drops";
 import "./ore_regeneration";
+import "./netherite_crafter_components";
 world.afterEvents.worldIntialize.subscribe(()=>{
     world.sendMessage("Scripting API initalized");
 });
